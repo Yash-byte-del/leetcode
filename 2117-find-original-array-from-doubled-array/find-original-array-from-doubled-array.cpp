41e352634708081734b436f2e93d89d1bc295cfd
Yash Karan <ykaran_be23@thapar.edu>
2026-02-05 21:23:59 +0530

    Time: 63 ms (94.50%) | Memory: 121 MB (98.01%) - LeetSync

---
+++ 2117-find-original-array-from-doubled-array/find-original-array-from-doubled-array.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> findOriginalArray(vector<int>& changed) {
+        int n = changed.size();
+        vector<int>result;
+        sort(begin(changed),end(changed));
+        for(int i=0;i<n;i++) {
+            int num = changed[i];
+            if(num < 0)continue;
+            auto idx = lower_bound(begin(changed)+i+1,end(changed),2*num);
+            if(idx!=end(changed) && *idx==2*num) {
+                *idx=-1;
+                result.push_back(num);
+            } else {
+                return {};
+            }
+        }
+        return result;
+    }
+};
